50757537549714382036d54ebf177d0de0e3a31d
André <andregcp01@gmail.com>
2025-11-30 07:40:12 -0300

    adicionada a pasta do primeiro bootcamp

---
+++ Python_Mastery_Bootcamp/2_Segundo_Programa/main.py
@@ -0,0 +1,8 @@
+v_inteiro = 10 # tipo inteiro
+v_real = 10.5 # tipo real, comumente chamada de "float" em Python
+v_string = "cadeia de caracteres" # string
+v_booleana = True # tipo booleana
+
+print(v_inteiro)
+
+print(type(v_booleana))
---
+++ Python_Mastery_Bootcamp/3_Terceiro_Programa/main.py
@@ -0,0 +1,17 @@
+A = 10
+B = 2
+C = 5
+
+print("A soma de A e B é", A + B) # dois argumentos
+
+X = 3
+Y = 2
+
+print("O valor de X + Y = {}".format(X + Y)) # as {} (chaves) puxam o valor .format(varável1 + variável2)
+
+V1 = 5
+v1 = -3
+
+print("O resultado da soma de {0} + {1} = {2}".format(V1, v1, V1 + v1)) # a caisa alta IMPORTA, V1 e v1, são duas coisas diferentes
+                                                                        # as {} (chaves) respeitam a ordem das variáveis que criamos
+
